2a11977398d8c3a3fda66f3ab2fb73ce68684f2a
yusei0329 <nwys.28@gmail.com>
2021-04-29 15:44:51 +0900

    更新

---
+++ 03/issue/src/UtilityMethods.java
@@ -0,0 +1,14 @@
+public class UtilityMethods {
+    void printCurrency(double value){
+        return ;
+    }
+
+    public static void main(String[] args) {
+        UtilityMethods app = new UtilityMethods();
+
+        //動作確認1-1
+        System.out.println("1-1 動作確認----------------------");
+
+        //動作確認1-2
+    }
+}
