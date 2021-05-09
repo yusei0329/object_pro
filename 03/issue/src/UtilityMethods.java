4f3aff3aa824131b56ac6cfeb6ead956089cab6f
yusei0329 <nwys.28@gmail.com>
2021-05-09 14:04:40 +0900

    更新

--- 03/issue/src/UtilityMethods.java
+++ 03/issue/src/UtilityMethods.java
@@ -1,14 +1,25 @@
 public class UtilityMethods {
     void printCurrency(double value){
-        return ;
+        System.out.printf("%.2f\n", value);
+    }
+    int add(int a, int b){
+        return a + b;
+    }
+    double division(int a, int b){
+        return (double) a / (double) b;
+    }
+
+    String getTheAnswerToTheUltimateQuestionOfLife_TheUniverse_And_Everything(){
+        try{
+            Thread.sleep(750);
+        }catch(InterruptedException e){}
+        return "42";
     }
 
     public static void main(String[] args) {
         UtilityMethods app = new UtilityMethods();
-
-        //動作確認1-1
-        System.out.println("1-1 動作確認----------------------");
-
-        //動作確認1-2
+        System.out.println(app.add(1,2));
+        System.out.println(app.division(5,2));
+        System.out.println(app.getTheAnswerToTheUltimateQuestionOfLife_TheUniverse_And_Everything());
     }
 }
